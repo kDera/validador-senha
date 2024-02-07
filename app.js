e2d0f18eb682819ef034ba785c2ecec98318e9da
kDera <caduvazquezabj@gmail.com>
2024-02-07 17:35:29 -0300

    finalizado

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function pswValidation(){
     let senha = document.getElementById("password").value;
+    let resultado = document.getElementById("resultado")
     
 
     //validar tamanho minimo de 8 digitos
@@ -25,4 +26,20 @@ function pswValidation(){
         console.log("A senha tem MAIÚSCULA? "+letraMai);
         console.log("A senha tem numeral? "+Num);
         console.log("A senha tem caractere especial? "+carcEsp);
+    
+    document.getElementById("resultado").innerText
+
+    if(pswSize && letraMin && letraMai && Num && carcEsp){
+        document.getElementById("resultado").innerText = "a senha cumpre com todos os requisitos";
+    }
+    else{
+        msg = document.getElementById("resultado").innerText= "a senha não cumpre os seguintes requisitos:" //mostar o que falta
+        //se o tam for menor que 8, mostrar
+        if(!pswSize) {document.getElementById("resultado").innerText += "\n  não atingiu os 8 dígitos"}
+        if(!letraMin) {document.getElementById("resultado").innerText += "\n  não tem letra minuscula"}
+        if(!letraMai) {document.getElementById("resultado").innerText += "\n  não tem letra maiuscula"}
+        if(!Num) {document.getElementById("resultado").innerText += "\n  não tem numeral"}
+        if(!carcEsp) {document.getElementById("resultado").innerText += "\n não tem caractere especial"}
+        
+    }
 }
